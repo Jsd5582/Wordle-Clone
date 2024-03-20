d2240d11de1e0c574fd456ca34a0c5adfe13a93e
Jaime <jdiaz5582@g.rit.edu>
2024-03-19 22:43:23 -0400

    Progress made in the logic of the program a bit of a bug removing the letters that are no longer available to the user

--- src/main.py
+++ src/main.py
@@ -2,32 +2,43 @@
 
 import random
 
+"""
+Note to self: Running into a bug where letters arent being removed from the Available letters list even if they aren't in the answer word. Check the loop in lines 23-29.
+"""
+
 
 '''Global variables'''
-AVAILABLE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ'
-unavailable_letters = ''
+Available_Letters = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ'
 WORD_LIST = ['Tiger','Cloud', 'Ocean', 'Chair', 'Ghost', 'Knife', 'Mouse', 'Plant', 'Queen', 'Radar', 'Stone', 'Brush', 'Apple',
             'Toast', 'Wagon', 'Llama', 'House', 'Clown', 'Crane', 'Flame']
 LIVES = 5
 
 def shared_letters(user_word, actual_word, unavailable_letters) -> list:
-    guess = [*user_word]
+    cap_word = user_word.upper()
+    guess = [*cap_word]
     answer = [*actual_word]
+    unavailable = [*unavailable_letters]
     shared = []
 
     for i in range(len(answer)):
+        '''If the elements match they are added to the list named shared'''
         if guess[i] == answer[i]:
             shared.append(answer[i])
         else:
-            unavailable_letters = unavailable_letters + guess[i].upper()
+            unavailable.remove(guess[i])
+            shared.append('-')
 
-    return shared
+    return shared,unavailable
 
 def wordle(word):
-    unavailable_letters = ''
+
+    '''Just a place holder to see the picked word to ensure that the algorithm is preforming as intended'''
+    print(word)
+
     for i in range(LIVES):
-        print(shared_letters('house',word, unavailable_letters))
-        
+        show, new_avail = shared_letters('house',word, Available_Letters)
+        print(show)
+        print(new_avail)        
 
 def word_picker() -> str:
     '''
@@ -40,13 +51,14 @@ def word_picker() -> str:
     '''
     max = len(WORD_LIST)
     word = WORD_LIST[random.randint(0,max)]
-
-    return word
+    
+    """Word is returned capitalized to ensure that capitalization from the user is not a facotr in whether the systemm determines if the input is correct for not"""
+    return word.upper()
 
 def main():
-    print (AVAILABLE_LETTERS)
-    print ( word_picker() )
-    wordle( word_picker() )
+    print ("All available letters are: " + Available_Letters)
+    picked_word = word_picker() 
+    wordle( picked_word )
 
 if __name__ == '__main__':
     main()
